cf77d3792988731434adebb3ee08876149137406
Keivan Vosoughi <vkeivan@gmail.com>
2018-05-15 03:54:53 -0700

    Updatl for the Photo Slider

--- app/index.js
+++ app/index.js
@@ -9,6 +9,13 @@ import './styles/main.sass';
 import '!file-loader?name=[name].[ext]!./images/ace-builder1.png';
 import '!file-loader?name=[name].[ext]!./images/ace-builder2.png';
 import '!file-loader?name=[name].[ext]!./images/ace-builder3.png';
+import '!file-loader?name=[name].[ext]!./images/node-list1.png';
+import '!file-loader?name=[name].[ext]!./images/node-list2.png';
+import '!file-loader?name=[name].[ext]!./images/node-list3.png';
+import '!file-loader?name=[name].[ext]!./images/file-browser1.png';
+
+import '!file-loader?name=[name].[ext]!./images/file-browser3.png';
+import '!file-loader?name=[name].[ext]!./images/file-browser4.png';
 
 ReactDOM.render(
 <Provider store={appStore}>
